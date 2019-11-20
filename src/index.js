efccc332bf4d8291e0417dc7854ed8215c118868
Hugo Quispe <hugo.quispe96@outlook.es>
2019-11-20 18:05:08 -0300

    Primera_iteracion
    Login de usuarios

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const passport = require('passport');
+const flash = require('connect-flash');
+const MySQLStore = require('express-mysql-session')(session);
+const bodyParser = require('body-parser');
+
+const { database } = require('./keys');
+
 // Intializations
 const app = express();
+require('./lib/passport');
 
 // Settings
 app.set('port', process.env.PORT || 4000);
@@ -19,14 +28,26 @@ app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'db_drugo',
+  resave: false,
+  saveUninitialized: false,
+  store: new MySQLStore(database)
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Global variables
 app.use((req, res, next) => {
-
-    next();
-  });
+  app.locals.message = req.flash('message');
+  app.locals.success = req.flash('success');
+  app.locals.user = req.user;
+  next();
+});
 
 // Routes
 app.use(require('./routes/index'));
@@ -34,8 +55,9 @@ app.use(require('./routes/authentication'));
 app.use('/links', require('./routes/links'));
 
 // Public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting
 app.listen(app.get('port'), () => {
-    console.log('Server is in port', app.get('port'));
-  });
+  console.log('Server is in port', app.get('port'));
+});
